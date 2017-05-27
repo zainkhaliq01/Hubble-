937f128a992beb17a5f6cab5ebd11fd98a0b1ea5
Zain Khaliq <zkhaliqdev01@gmail.com>
2017-05-27 01:30:08 -0400

    Sign In and Sign Up screen Story Board ready (Backend Connection still needed)

---
+++ Hubble/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  Hubble
+//
+//  Created by Zain Khaliq on 5/26/17.
+//  Copyright © 2017 Hubble. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dissmiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
